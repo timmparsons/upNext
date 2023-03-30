a9c78433406493c81ab9ae0b1086a170664854a8
Tim Parsons <timmparsons85@gmail.com>
2023-03-30 07:19:33 -0700

    styling images

--- components/YourMovies.js
+++ components/YourMovies.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, ScrollView, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import { MOVIE_LIST } from '../constants/movieList';
 
 const YourMovies = () => {
-	console.log('qqq', MOVIE_LIST)
+	const [selectedMovies, setSelectedMovies] = useState([]);
+
+	const selectMovie = (movie) => {
+		if(selectedMovies.includes(movie.id)) return
+		const uniqueIds = new Set(selectedMovies)
+		setSelectedMovies([uniqueIds, movie.id])
+	}
+
+	console.log(selectedMovies)
+	const getSelectedMovie = (movie) => {
+		return selectedMovies.includes(movie.id);
+	}
+
 	return (
 		<ScrollView
 			contentContainerStyle={{
@@ -14,11 +26,15 @@ const YourMovies = () => {
 			showHorizontalScrollIndicator={false}
 		>
 			{MOVIE_LIST.map(movie => (
-				<TouchableOpacity style={styles.button} onPress={() => console.log('Press')}>
-				<Image 
-					key={movie.id}
-					style={styles.image}
-					source={{uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`}}/>
+				<TouchableOpacity 
+					style={styles.button}
+					onPress={() => selectMovie(movie)}>
+					<View style={styles.imageBorder}>
+					<Image 
+						key={movie.id}
+						style={styles.image}
+						source={{uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`}}/>
+					</View>
 				</TouchableOpacity>
 			))
 			}
@@ -28,14 +44,25 @@ const YourMovies = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    padding: 10,
+		flexDirection: 'row',
+		marginRight: 10
   }, 
   image: {
     height: 200,
     aspectRatio: 1/2,
-    marginHorizontal: 15
-  }
+	}, 
+	overlay: {
+		position: 'absolute',
+		width: '75%',
+		height: '100%',
+		backgroundColor: 'rgba(0,0,0,0.4)',
+		top: 0,
+		left: 0
+	},
+	imageBorder: {
+		borderWidth: 1,
+		borderColor: 'lightgray'
+	}
 });
 
 export default YourMovies
